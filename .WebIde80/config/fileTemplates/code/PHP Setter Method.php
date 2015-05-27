e680e13c19042a442e4b237d6b639451e50691d0
Benoit GALATI <benoit.galati@gmail.com>
2015-05-27 07:38:24 +0200

    Maj + nettoyage config PhpStorm

---
+++ .WebIde80/config/fileTemplates/code/PHP Setter Method.php
@@ -0,0 +1,13 @@
+/**
+ * @param ${TYPE_HINT} $${PARAM_NAME}
+ * @return $this
+ */
+public ${STATIC} function set${NAME}($${PARAM_NAME})
+{
+#if (${STATIC} == "static")
+    self::$${FIELD_NAME} = $${PARAM_NAME};
+#else
+    $this->${FIELD_NAME} = $${PARAM_NAME};
+#end
+    return $this;
+}
