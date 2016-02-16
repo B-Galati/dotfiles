80d9905d17bf4e7a106573cc54e322d2857308cd
Benoit GALATI <benoit.galati+github@gmail.com>
2016-02-16 08:47:12 +0100

    Expands tab to space

--- bootstrap.sh
+++ bootstrap.sh
@@ -78,10 +78,10 @@ if [[ "$1" == "--force" || "$1" == "-f" ]]; then
 	doIt
 else
     read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt
+    fi
 fi
 
 unset doIt
